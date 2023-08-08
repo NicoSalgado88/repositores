e373fd42b408406f05e801063b3b9d007df9834a
NicoSalgado88 <nicolas_88191@hotmail.com>
2023-08-08 17:25:58 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/repositores/Repositores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositores;
+
+/**
+ *
+ * @author Nicol
+ */
+public class Repositores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
